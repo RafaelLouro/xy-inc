ef664cf68e9d6d52434466e45f2a1acbd2c2cbd4
Rafael Louro <rlourox@gmail.com.br>
2020-08-16 15:05:12 -0300

    Projeto base

---
+++ src/main/kotlin/test/zup/rafael/xyinc/XyincApplication.kt
@@ -0,0 +1,11 @@
+package test.zup.rafael.xyinc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XyincApplication
+
+fun main(args: Array<String>) {
+	runApplication<XyincApplication>(*args)
+}
